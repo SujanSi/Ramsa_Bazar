076e9b5baf45ef9c794f7906a7a52300a26e62f1
shyam <shyam.s.ajira@gmail.com>
2025-03-20 14:46:09 +0545

    change rge & login page desgine

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,6 @@ app_name = 'customer'
 
 urlpatterns = [
     
-    path('index/', index, name='index'),
     path('kyc-verification/', kyc_verification, name='kyc_verification'),
     path('profile/', profile, name='profile'),
     path('edit-profile/', edit_profile, name='edit_profile'),
